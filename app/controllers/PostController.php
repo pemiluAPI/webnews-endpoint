223df25554e78ea9e58558aba0b1b431016ab037
Hendy <hendy.yanuar@kiranatama.com>
2015-02-11 17:04:27 +0700

    Simplisize API parser from news to XApi

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -19,24 +19,12 @@ class PostController extends BaseController {
 		$limit = Input::get('limit', 100);
 		$offset = Input::get('offset', 0);
 
-		return $this->apiParser( $this->post->allPostsPaged($limit, $offset) );
+		return XApi::parser( $this->post->allPostsPaged($limit, $offset) );
 	}
 
 	public function getOne($id)
 	{
-		return $this->apiParser( $this->post->onePost($id) );
+		return XApi::parser( $this->post->onePost($id) );
 	}
 
-	private function apiParser($posts, $error = 0)
-	{
-		// Result Template
-		$results = array();
-		$results['count'] = count($posts);
-		$results['data'] = $posts;
-
-		return XApi::response([
-			'results' => $results,
-			'error' => $error
-		]);
-	}
 }
--- app/library/XApi.php
+++ app/library/XApi.php
@@ -2,13 +2,26 @@
 
 class XApi
 {
-    public static function response($data = array('error' => 0, 'results' => null), $http_code = 200)
-    {
-        return Response::json(
-            array(
-                'error' =>   $data['error'],
-                'results' => $data['results'],
-            ),
-            $http_code);
-    }
+	public static function response($data = array('error' => 0, 'results' => null), $http_code = 200)
+	{
+		return Response::json(
+			array(
+				'error' =>   $data['error'],
+				'results' => $data['results'],
+			),
+			$http_code);
+	}
+
+	public static function parser($datas, $error = 0)
+	{
+		// Result Template
+		$results = array();
+		$results['count'] = count($datas);
+		$results['data'] = $datas;
+
+		return XApi::response([
+			'results' => $results,
+			'error' => $error
+		]);
+	}
 }
