f245c3538112f6e972e01f613bab16c680991337
Hendy <hendy.yanuar@kiranatama.com>
2015-02-12 15:10:04 +0700

    Setup default config file template in .env.DIST.php

---
+++ .env.DIST.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * DIST SETTINGS
+ *
+ * Copy, rename to .env.ENVIRONMENT.php, and configure accordingly. For
+ * local sandboxes, rename to .env.local.php. For production, rename to
+ * .env.production.php.
+ *
+ * See http://laravel.com/docs/4.2/configuration#protecting-sensitive-configuration
+ * for details.
+ */
+return array(
+	/* Database */
+	'DATABASE_HOST'     => 'localhost',
+	'DATABASE_NAME'     => 'NAME',
+	'DATABASE_USER'     => 'USER',
+	'DATABASE_PASSWORD' => 'PASSWORD'
+);
